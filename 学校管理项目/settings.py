8de925a199b06bdfa03ec6e2398bcd466476992c
Paul <287618817@qq.com>
2019-05-09 14:28:31 +0800

    后台用户分配用户组开始

--- 学校管理项目/settings.py
+++ 学校管理项目/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = 'i0kf%ls9254v(l1*9g4ex&7q-3r5zj5fo-+fhyc2$umy#*x-ne'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daydayupyuan.cn']
-#     '127.0.0.1',
-#     '176.209.106.46',
-#     '192.168.2.100',
-#     '0.0.0.0',
-# ]
+ALLOWED_HOSTS = ['daydayupyuan.cn'
+    '127.0.0.1',
+    '176.209.106.46',
+    '192.168.2.100',
+    '0.0.0.0',
+]
 
 
 # Application definition
